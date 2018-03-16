8ba65cdee751d7a9fe2a7739c79aff580fa122b6
Marco T.C. (BadWolf) <marco@badwolf.one>
2018-03-16 10:10:01 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,30 +1,32 @@
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := KIRIN
+TARGET_BOOTLOADER_BOARD_NAME := hi3660
 TARGET_NO_BOOTLOADER := true
 
 # Platform
-TARGET_BOARD_PLATFORM := generic
-TARGET_BOARD_PLATFORM_GPU := kirin
+TARGET_BOARD_PLATFORM := hi3660
+TARGET_BOARD_PLATFORM_GPU := mali-g71
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
-BOARD_KERNEL_CMDLINE := 
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
-BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_CMDLINE := loglevel=4 initcall_debug=n page_tracker=on slub_min_objects=16 androidboot.selinux=permissive
+
+BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x8000 --ramdisk_offset 0x01000000 --tags_offset 0x0100
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --second_offset 0x00e88000 --tags_offset 0x07988000
 
 # phony empty kernel to satisfy build system, but this device does not
 # include a kernel in the recovery image -- flash to recovery_ramdisk
@@ -33,26 +35,37 @@ TARGET_PREBUILT_KERNEL := device/huawei/berkeley/dummykernel
 # TARGET_KERNEL_SOURCE := kernel/huawei/berkeley
 # TARGET_KERNEL_CONFIG := berkeley_defconfig
 
-# NOTE I only bother properly setting recovery image size
-BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE		:= 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE	:= 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE	:= 4915724288
+BOARD_USERDATAIMAGE_PARTITION_SIZE	:= 56547606528
+BOARD_CACHEIMAGE_PARTITION_SIZE		:= 268435456
+BOARD_FLASH_BLOCK_SIZE			:= 4096
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR	:= true
 
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 TW_THEME := portrait_hdpi
 BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB565"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd_backlight0/brightness
 TW_NO_SCREEN_BLANK := true
 TW_DEFAULT_BRIGHTNESS := "2048"
 TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.gs6/lun.0/file"
+TW_MAX_BRIGHTNESS := 7992
+TW_DEFAULT_BRIGHTNESS := 1573
+TW_INCLUDE_NTFS_3G := true
 # Device crashes if /sbin/modprobe is present so this is needed:
-BOARD_CUSTOM_BOOTIMG_MK := device/huawei/berkeley/custombootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := device/huawei/vky/custombootimg.mk
 # MTP will not work until we update it to support ffs
 TW_EXCLUDE_MTP := true
+TW_EXCLUDE_SUPERSU := true
+# TW_INCLUDE_CRYPTO := true
+# TW_INCLUDE_CRYPTO_FBE := true
